141819efbe3e25b8e1813b6f49f05d685d4f69bc
Robbie McCorkell <robbiemccorkell@gmail.com>
2015-06-20 21:33:00 +0100

    Try prettifying markdown

--- build-docs.sh
+++ build-docs.sh
@@ -1,9 +1,16 @@
+npm install -g tidy-markdown
+
 # Checkout website
 git clone git@github.com:arch-js/arch-js.github.io.git
 
+echo $PWD
 # Copy Docs
-
 # Run through beautifier
+for file in ./docs/*
+do
+  tidy-markdown < $file > ../arch-js.github.io/docs/$file.md
+done
+
 
 # Add frontmatter
 
